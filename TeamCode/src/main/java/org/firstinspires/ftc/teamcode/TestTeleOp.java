e54f85fb24d1636b69b42961445b409fcf3dccdb
10650biohazards <hazmat10650@gmail.com>
2025-09-10 16:33:35 -0400

    sample TeleOp code
    The sample TeleOp code for reference on basic FTC operation later.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -2,13 +2,41 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "First Teleop")
 public class TestTeleOp extends LinearOpMode {
 
+    DcMotor fleft, fright, bright, bleft;
+
     @Override
     public void runOpMode() throws InterruptedException {
 
-    }
+        fleft = hardwareMap.get(DcMotor.class, "fleft");
+        fright = hardwareMap.get(DcMotor.class, "fright");
+        bright = hardwareMap.get(DcMotor.class, "bright");
+        bleft = hardwareMap.get(DcMotor.class, "bleft");
 
+
+        while(opModeIsActive()) {
+
+//            if(gamepad1.a) {
+//                // move robot
+//                fleft.setPower(1);
+//            } else {
+//                // stop robot
+//                fleft.setPower(0);
+//            }
+
+            if(Math.abs(gamepad2.left_stick_y) == 0.2) {
+                fleft.setPower(gamepad2.left_stick_y);
+            } else {
+                fleft.setPower(0);
+            }
+
+        }
+
+
+    }
 }
