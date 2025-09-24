035f96667a6763d939b028a4e176d02dc9f1cc23
10650biohazards <hazmat10650@gmail.com>
2025-09-24 17:02:25 -0400

    Team update for auto code
    This code updated robot auto drive forward for millimeter input and encoder input

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -12,7 +12,9 @@ public class TestAuto extends LinearOpMode {
     DcMotor fleft, fright, bright, bleft;
     Servo servo;
 
-    double power = 0.5;
+    final int WHEEL_DIAMETER = 96;
+    final double WHEEL_ENCODER_RESOLUTION = 384.5;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -25,26 +27,78 @@ public class TestAuto extends LinearOpMode {
         bright.setDirection(DcMotorSimple.Direction.REVERSE);
         fright.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        // Reset ALLLL of the encoders
+        fright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         waitForStart();
         // steps!
 
+        // Step 1: Drive forward at a certain speed
+        // Step 2: Wait UNTIL encoders reach a specified value
+        // Step 3: Stop
+//        drivingForward(500, .2);
 
-        fleft.setPower(power);
-        fright.setPower(power);
-        bright.setPower(power);
-        bleft.setPower(power);
-
-        waitTime(1000);
-
-        fleft.setPower(0);
-        fright.setPower(0);
-        bleft.setPower(0);
-        bright.setPower(0);
+        drivingForwardMM(1000,0.2);
     }
 
     public void waitTime (int time) {
         long targetTime = System.currentTimeMillis()+time;
         while(opModeIsActive() && System.currentTimeMillis()<targetTime) {}
+    }
+    public void drivingForward (int target, double power) {
+
+
+
+        target += averageTicks();
+        while (Math.abs(target-averageTicks()) > 5) {
+            int sign = (target-averageTicks())/Math.abs(target-averageTicks());
+            fleft.setPower(power*sign);
+            fright.setPower(power*sign);
+            bright.setPower(power*sign);
+            bleft.setPower(power*sign);
+
+            telemetry.addData("Encoder",averageTicks());
+            telemetry.addData("Power",power*sign);
+            telemetry.update();
+
+        }
+
+        fleft.setPower(0);
+        fright.setPower(0);
+        bright.setPower(0);
+        bleft.setPower(0);
+
+    }
+
+    public int averageTicks (){
+        return (fleft.getCurrentPosition()+ fright.getCurrentPosition()+ bright.getCurrentPosition()+ bleft.getCurrentPosition())/4;
+    }
+
+    public void drivingForwardMM(int targetMillimeters, double power) {
+        double circumference = WHEEL_DIAMETER*Math.PI;
+        int targetTicks = (int)(targetMillimeters*(WHEEL_ENCODER_RESOLUTION/circumference));
+
+        drivingForward(targetTicks, power);
+    }
+    public void driveUntilMilli (int milli, double power) {
+        fleft.setPower(power);
+        fright.setPower(power);
+        bright.setPower(power);
+        bleft.setPower(power);
+
+        waitTime(milli);
+
+        fleft.setPower(0);
+        fright.setPower(0);
+        bleft.setPower(0);
+        bright.setPower(0);
     }
 
 
