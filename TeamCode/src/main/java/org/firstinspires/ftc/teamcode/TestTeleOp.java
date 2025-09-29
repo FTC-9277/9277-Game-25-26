c0b84f7f94ed01f8e57e10b3476fc001b0fdb00b
10650biohazards <hazmat10650@gmail.com>
2025-09-29 16:42:28 -0400

    added testmotor
    this update added the test control of flying wheel.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -3,26 +3,38 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "First Teleop")
 public class TestTeleOp extends LinearOpMode {
 
     DcMotor fleft, fright, bright, bleft;
+    DcMotorEx testMotor;
     Servo servo;
+
+    double speed = 0;
+
+    public ElapsedTime time = new ElapsedTime();
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         fleft = hardwareMap.get(DcMotor.class, "fleft");
-        fright = hardwareMap.get(DcMotor.class, "fright");
+        //fright = hardwareMap.get(DcMotor.class, "fright");
         bright = hardwareMap.get(DcMotor.class, "bright");
         bleft = hardwareMap.get(DcMotor.class, "bleft");
         servo = hardwareMap.get(Servo.class, "serv");
         bright.setDirection(DcMotorSimple.Direction.REVERSE);
-        fright.setDirection(DcMotorSimple.Direction.REVERSE);
+        //fright.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        //this is for testing of drive wheel
+        testMotor = hardwareMap.get(DcMotorEx.class, "testMotor");
+        testMotor.setVelocity(0);
+        testMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        testMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
         waitForStart();
         while(opModeIsActive()) {
@@ -34,12 +46,12 @@ public class TestTeleOp extends LinearOpMode {
 
             if(Math.abs(gamepad2.left_stick_y) >= 0.2 || Math.abs(gamepad2.left_stick_x) >= 0.2 || Math.abs(gamepad2.right_stick_x) >= 0.2) {
                 fleft.setPower(speed * (gamepad2.left_stick_y-gamepad2.right_stick_x+gamepad2.left_stick_x));
-                fright.setPower(speed * (gamepad2.left_stick_y+gamepad2.right_stick_x-gamepad2.left_stick_x));
+                //fright.setPower(speed * (gamepad2.left_stick_y+gamepad2.right_stick_x-gamepad2.left_stick_x));
                 bright.setPower (speed * (gamepad2.left_stick_y+gamepad2.right_stick_x+gamepad2.left_stick_x));
                 bleft.setPower(speed * (gamepad2.left_stick_y-gamepad2.right_stick_x-gamepad2.left_stick_x));
             } else {
                 fleft.setPower(0);
-                fright.setPower(0);
+                //fright.setPower(0);
                 bright.setPower(0);
                 bleft.setPower(0);
             }
@@ -51,7 +63,22 @@ public class TestTeleOp extends LinearOpMode {
                 servo.setPosition(-1);
             }
 
+            while(gamepad2.b) {
 
+                if (time.seconds() <= 10){
+                    speed = 6*time.seconds();
+                    testMotor.setVelocity(speed);
+                    telemetry.addData("Motor speed",speed);
+                    telemetry.addData("Time", time);
+
+                    telemetry.update();
+                } else {
+                    testMotor.setVelocity(60);
+                }
+
+            }
+            time.reset();
+            testMotor.setVelocity(0);
 
 
         }
