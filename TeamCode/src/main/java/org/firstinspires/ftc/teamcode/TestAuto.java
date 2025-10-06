3f1e765e431eb8b5146a28da08317bae2edfbfcd
10650biohazards <hazmat10650@gmail.com>
2025-10-06 17:41:20 -0400

    code ported to Kevin Kevin robot
    ported code to kevin kevin. Programed launcher system. Added basic auto turning. Added auto m/s to t/s conversion.
    Kevin Kevin knows all.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -4,6 +4,7 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -11,15 +12,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @Autonomous(name = "TestAuto")
 public class TestAuto extends LinearOpMode {
 
-    DcMotor fleft, fright, bright, bleft;
+    DcMotorEx fleft, fright, bright, bleft;
     Servo servo;
 
     IMU imu;
     public double imuDegree = 0;
+
+
 
     final int WHEEL_DIAMETER = 96;
     final double WHEEL_ENCODER_RESOLUTION = 384.5;
@@ -32,23 +36,28 @@ public class TestAuto extends LinearOpMode {
         RevHubOrientationOnRobot orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD);
         imu.initialize(new IMU.Parameters(orientation));
 
-        fleft = hardwareMap.get(DcMotor.class, "fleft");
-        fright = hardwareMap.get(DcMotor.class, "fright");
-        bright = hardwareMap.get(DcMotor.class, "bright");
-        bleft = hardwareMap.get(DcMotor.class, "bleft");
+
+        imu.resetYaw();
+
+
+
+        fleft = hardwareMap.get(DcMotorEx.class, "fleft");
+        fright = hardwareMap.get(DcMotorEx.class, "fright");
+        bright = hardwareMap.get(DcMotorEx.class, "bright");
+        bleft = hardwareMap.get(DcMotorEx.class, "bleft");
         servo = hardwareMap.get(Servo.class, "serv");
-        bright.setDirection(DcMotorSimple.Direction.REVERSE);
-        fright.setDirection(DcMotorSimple.Direction.REVERSE);
+        bright.setDirection(DcMotorEx.Direction.REVERSE);
+        fright.setDirection(DcMotorEx.Direction.REVERSE);
 
         // Reset ALLLL of the encoders
-        fright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        bright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        bleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        fleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        bright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        bleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fright.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        fleft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        bright.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        bleft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        fright.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        fleft.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        bright.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        bleft.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
         // steps!
@@ -58,52 +67,71 @@ public class TestAuto extends LinearOpMode {
         // Step 3: Stop
 //        drivingForward(500, .2);
 
-        //drivingForwardMM(1000,0.2);
-        while (opModeIsActive()) {
-            telemetry.addData("Heading",getHeading());
-            telemetry.update();
+        while(opModeIsActive()) {
+            while (opModeIsActive()) {
+                telemetry.addData("Heading", getHeading());
+                telemetry.update();
+            }
         }
+
+        //drivingForwardMM(1000,2000);
+
+
+
+
     }
-
-
 
     public double getHeading() {
 //        double changeDegree = imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).firstAngle;
-        double changeDegree = imu.getRobotYawPitchRollAngles().getYaw();
+        YawPitchRollAngles ang = imu.getRobotYawPitchRollAngles();
+        double changeDegree = ang.getYaw(AngleUnit.DEGREES);
+
+        telemetry.addData("yaw", imu.getRobotYawPitchRollAngles().getYaw());
         telemetry.addData("Change Degree",changeDegree);
+
         imuDegree += changeDegree;
-        imu.resetYaw();
+        //imu.resetYaw();
         return imuDegree;
     }
 
+    public void turmBy(double Deg, double maxPower, double kP) {
+        double start = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+        double target = AngleUnit.normalizeDegrees(start+Deg);
+
+        double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+
+        telemetry.addData("value",String.valueOf(start),String.valueOf(target), String.valueOf(heading));
+    }
 
     public void waitTime (int time) {
         long targetTime = System.currentTimeMillis()+time;
         while(opModeIsActive() && System.currentTimeMillis()<targetTime) {}
     }
-    public void drivingForward (int target, double power) {
+    public void drivingForward (int target, double velocity) {
+
+        //convert velocity to ticks
 
 
 
         target += averageTicks();
-        while (Math.abs(target-averageTicks()) > 5) {
+        while (Math.abs(target-averageTicks()) > 5 && opModeIsActive()) {
             int currentDistance = target-averageTicks();
             int sign = (currentDistance)/Math.abs(currentDistance);
-            fleft.setPower(power*sign);
-            fright.setPower(power*sign);
-            bright.setPower(power*sign);
-            bleft.setPower(power*sign);
+            fleft.setVelocity(velocity*sign);
+            fright.setVelocity(velocity*sign);
+            bright.setVelocity(velocity*sign);
+            bleft.setVelocity(velocity*sign);
 
             telemetry.addData("Encoder",averageTicks());
-            telemetry.addData("Power",power*sign);
+            telemetry.addData("Velocity",velocity*sign);
             telemetry.update();
 
         }
 
-        fleft.setPower(0);
-        fright.setPower(0);
-        bright.setPower(0);
-        bleft.setPower(0);
+        fleft.setVelocity(0);
+        fright.setVelocity(0);
+        bright.setVelocity(0);
+        bleft.setVelocity(0);
 
     }
 
@@ -111,28 +139,31 @@ public class TestAuto extends LinearOpMode {
         return (fleft.getCurrentPosition()+ fright.getCurrentPosition()+ bright.getCurrentPosition()+ bleft.getCurrentPosition())/4;
     }
 
-    public void drivingForwardINCH(double targetInches, double power) {
-        drivingForwardMM((int) Math.round(targetInches/0.0394),power);
+    public void drivingForwardINCH(double targetInches, double velocity) {
+        drivingForwardMM((int) Math.round(targetInches/0.0394),velocity);
     }
 
-    public void drivingForwardMM(int targetMillimeters, double power) {
+    public void drivingForwardMM(int targetMillimeters, double velocityMetersPerSec) {
         double circumference = WHEEL_DIAMETER*Math.PI;
-        int targetTicks = (int)(targetMillimeters*(WHEEL_ENCODER_RESOLUTION/circumference));
+        double ticksPerMilli = WHEEL_ENCODER_RESOLUTION/circumference;
+        double ticksPerMeter = ticksPerMilli / 1000;
+        int targetTicks = (int)(targetMillimeters*(ticksPerMilli));
 
-        drivingForward(targetTicks, power);
+        double ticksPerSec = ticksPerMeter * velocityMetersPerSec;
+        drivingForward(targetTicks, ticksPerSec);
     }
-    public void driveUntilMilli (int milli, double power) {
-        fleft.setPower(power);
-        fright.setPower(power);
-        bright.setPower(power);
-        bleft.setPower(power);
+    public void driveUntilMilli (int milli, double velocity) {
+        fleft.setVelocity(velocity) ;
+        fright.setVelocity(velocity) ;
+        bright.setVelocity(velocity) ;
+        bleft.setVelocity(velocity) ;
 
         waitTime(milli);
 
-        fleft.setPower(0);
-        fright.setPower(0);
-        bleft.setPower(0);
-        bright.setPower(0);
+        fleft.setVelocity(0) ;
+        fright.setVelocity(0) ;
+        bleft.setVelocity(0) ;
+        bright.setVelocity(0) ;
     }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -12,10 +12,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class TestTeleOp extends LinearOpMode {
 
     DcMotor fleft, fright, bright, bleft;
-    DcMotorEx testMotor;
-    Servo servo;
+    DcMotorEx shooter;
 
-    double speed = 0;
+    double shooterSpeed = 0;
 
     public ElapsedTime time = new ElapsedTime();
 
@@ -23,18 +22,18 @@ public class TestTeleOp extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         fleft = hardwareMap.get(DcMotor.class, "fleft");
-        //fright = hardwareMap.get(DcMotor.class, "fright");
+        fright = hardwareMap.get(DcMotor.class, "fright");
         bright = hardwareMap.get(DcMotor.class, "bright");
         bleft = hardwareMap.get(DcMotor.class, "bleft");
-        servo = hardwareMap.get(Servo.class, "serv");
         bright.setDirection(DcMotorSimple.Direction.REVERSE);
-        //fright.setDirection(DcMotorSimple.Direction.REVERSE);
+        fright.setDirection(DcMotorSimple.Direction.REVERSE);
 
         //this is for testing of drive wheel
-        testMotor = hardwareMap.get(DcMotorEx.class, "testMotor");
-        testMotor.setVelocity(0);
-        testMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        testMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        shooter = hardwareMap.get(DcMotorEx.class, "shooter");
+        shooter.setVelocity(0);
+        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        shooter.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
         while(opModeIsActive()) {
@@ -45,40 +44,35 @@ public class TestTeleOp extends LinearOpMode {
 
 
             if(Math.abs(gamepad2.left_stick_y) >= 0.2 || Math.abs(gamepad2.left_stick_x) >= 0.2 || Math.abs(gamepad2.right_stick_x) >= 0.2) {
-                fleft.setPower(speed * (gamepad2.left_stick_y-gamepad2.right_stick_x+gamepad2.left_stick_x));
-                //fright.setPower(speed * (gamepad2.left_stick_y+gamepad2.right_stick_x-gamepad2.left_stick_x));
-                bright.setPower (speed * (gamepad2.left_stick_y+gamepad2.right_stick_x+gamepad2.left_stick_x));
-                bleft.setPower(speed * (gamepad2.left_stick_y-gamepad2.right_stick_x-gamepad2.left_stick_x));
+                fleft.setPower(speed * (gamepad2.left_stick_y-gamepad2.right_stick_x-gamepad2.left_stick_x));
+                fright.setPower(speed * (gamepad2.left_stick_y+gamepad2.right_stick_x+gamepad2.left_stick_x));
+                bright.setPower (speed * (gamepad2.left_stick_y+gamepad2.right_stick_x-gamepad2.left_stick_x));
+                bleft.setPower(speed * (gamepad2.left_stick_y-gamepad2.right_stick_x+gamepad2.left_stick_x));
             } else {
                 fleft.setPower(0);
-                //fright.setPower(0);
+                fright.setPower(0);
                 bright.setPower(0);
                 bleft.setPower(0);
             }
 
 
-            if(gamepad2.y) {
-                servo.setPosition(1);
-            } else {
-                servo.setPosition(-1);
-            }
 
             while(gamepad2.b) {
 
-                if (time.seconds() <= 10){
-                    speed = 6*time.seconds();
-                    testMotor.setVelocity(speed);
-                    telemetry.addData("Motor speed",speed);
+                if (time.seconds() <= 5){
+                    shooterSpeed = 320*time.seconds();
+                    shooter.setVelocity(shooterSpeed);
+                    telemetry.addData("Motor speed",shooter.getVelocity());
                     telemetry.addData("Time", time);
 
                     telemetry.update();
                 } else {
-                    testMotor.setVelocity(60);
+                    shooter.setVelocity(1600);
                 }
 
             }
             time.reset();
-            testMotor.setVelocity(0);
+            shooter.setVelocity(0);
 
 
         }
